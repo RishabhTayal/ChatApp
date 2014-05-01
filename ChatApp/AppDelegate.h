72ca0ac053ab335470212e6cdce6f10ea05c5a47
Rishabh Tayal <rtayal11@gmail.com>
2014-05-01 12:33:49 -0500

    Initial Commit

---
+++ ChatApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ChatApp
+//
+//  Created by Rishabh Tayal on 5/1/14.
+//  Copyright (c) 2014 Rishabh Tayal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ChatApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ChatApp
+//
+//  Created by Rishabh Tayal on 5/1/14.
+//  Copyright (c) 2014 Rishabh Tayal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
