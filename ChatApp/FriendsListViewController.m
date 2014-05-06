9992a36b05b29c48a5140e99aa7a9ba793df4743
Rishabh Tayal <rtayal11@gmail.com>
2014-05-06 13:22:56 -0500

    commit.

--- ChatApp/FriendsListViewController.m
+++ ChatApp/FriendsListViewController.m
@@ -92,18 +92,12 @@
     
     // Configure the cell...
     cell.friendName.text = _friendsUsingApp[indexPath.row][@"name"];
-    NSDictionary *params = [NSDictionary dictionaryWithObjectsAndKeys: NO, @"redirect", @"200", @"height", @"normal", @"type", @"200", @"width", nil];
+    NSDictionary *params = [NSDictionary dictionaryWithObjectsAndKeys: NO, @"redirect", @"50", @"height", @"normal", @"type", @"50", @"width", nil];
+    
     /* make the API call */
-    [FBRequestConnection startWithGraphPath:[NSString stringWithFormat:@"/%@/picture", _friendsUsingApp[indexPath.row][@"id"]]
-                                 parameters:params
-                                 HTTPMethod:@"GET"
-                          completionHandler:^(
-                                              FBRequestConnection *connection,
-                                              id result,
-                                              NSError *error
-                                              ) {
-                              [cell.profilePicture setImageWithURL:connection.urlResponse.URL];
-                          }];
+    [FBRequestConnection startWithGraphPath:[NSString stringWithFormat:@"/%@/picture", _friendsUsingApp[indexPath.row][@"id"]] parameters:params HTTPMethod:@"GET" completionHandler:^(FBRequestConnection *connection, id result, NSError *error) {
+        [cell.profilePicture setImageWithURL:connection.urlResponse.URL];
+    }];
     return cell;
 }
 
@@ -117,54 +111,5 @@
     chatVC.friendId = _friendsUsingApp[indexPath.row][@"id"];
     [self.navigationController pushViewController:chatVC animated:YES];
 }
-
-/*
- // Override to support conditional editing of the table view.
- - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
- {
- // Return NO if you do not want the specified item to be editable.
- return YES;
- }
- */
-
-/*
- // Override to support editing the table view.
- - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
- {
- if (editingStyle == UITableViewCellEditingStyleDelete) {
- // Delete the row from the data source
- [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
- } else if (editingStyle == UITableViewCellEditingStyleInsert) {
- // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
- }
- }
- */
-
-/*
- // Override to support rearranging the table view.
- - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
- {
- }
- */
-
-/*
- // Override to support conditional rearranging of the table view.
- - (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
- {
- // Return NO if you do not want the item to be re-orderable.
- return YES;
- }
- */
-
-/*
- #pragma mark - Navigation
- 
- // In a storyboard-based application, you will often want to do a little preparation before navigation
- - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
- {
- // Get the new view controller using [segue destinationViewController].
- // Pass the selected object to the new view controller.
- }
- */
 
 @end
