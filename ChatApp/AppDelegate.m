122ee10c4dc362a8a2496b9b8d77fd2db7c1f777
Rishabh Tayal <rtayal11@gmail.com>
2014-06-05 13:58:05 -0500

    Added [Facebook activateApp] to track app install.

--- ChatApp/AppDelegate.m
+++ ChatApp/AppDelegate.m
@@ -168,8 +168,10 @@
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
-    
     [FBAppCall handleDidBecomeActiveWithSession:[PFFacebookUtils session]];
+    
+    // Register App Install on Facebook Ads Manager
+    [FBAppEvents activateApp];
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
