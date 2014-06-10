c227827f4cee67f8ea5e220396f99f543f8a0e35
Rishabh Tayal <rtayal11@gmail.com>
2014-06-10 12:31:21 -0500

    text correction in tutorial view.

--- ChatApp/AppDelegate.m
+++ ChatApp/AppDelegate.m
@@ -110,7 +110,7 @@
 
 -(void)setLoginView
 {
-    NSArray* infoArray = @[@{@"Header": @"Hanging out with Friends", @"Label": @"Chat with your Facebook Friends."}, @{@"Header": @"Camping with Family/Friends?", @"Label": @"Chat with nearby people even when no network is available."}, @{@"Header": @"Take it to the beach", @"Label": @"Make new friends at the beach."}, @{@"Header": @"Attending a Concert?", @"Label":@"Connect with other people."}, @{@"Header":@"Going to a Conference?", @"Label":@"Connect with other people seemlessly."}];
+    NSArray* infoArray = @[@{@"Header": @"Hanging out with Friends", @"Label": @"Chat with your Facebook Friends when Internet available."}, @{@"Header": @"Camping with Family/Friends?", @"Label": @"Chat with nearby people even when no network is available."}, @{@"Header": @"Take it to the beach", @"Label": @"Make new friends at the beach."}, @{@"Header": @"Attending a Concert?", @"Label":@"Connect with other people."}, @{@"Header":@"Going to a Conference?", @"Label":@"Connect with other people seemlessly."}];
     
     IntroViewController* intro = [[IntroViewController alloc] initWithBackgroundImages:@[@"Intro-bg-1", @"Intro-bg-2", @"Intro-bg-3", @"Intro-bg-4", @"Intro-bg-5"] andInformations:infoArray];
     
