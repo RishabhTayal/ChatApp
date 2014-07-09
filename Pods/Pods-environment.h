5b4f572b09756832c1eceb3079a5eec211811b20
Rishabh Tayal <rtayal11@gmail.com>
2014-07-09 15:09:08 -0500

    commit

--- Pods/Pods-environment.h
+++ Pods/Pods-environment.h
@@ -70,7 +70,7 @@
 #define COCOAPODS_POD_AVAILABLE_Facebook_iOS_SDK
 #define COCOAPODS_VERSION_MAJOR_Facebook_iOS_SDK 3
 #define COCOAPODS_VERSION_MINOR_Facebook_iOS_SDK 15
-#define COCOAPODS_VERSION_PATCH_Facebook_iOS_SDK 0
+#define COCOAPODS_VERSION_PATCH_Facebook_iOS_SDK 1
 
 // GoogleAnalytics-iOS-SDK
 #define COCOAPODS_POD_AVAILABLE_GoogleAnalytics_iOS_SDK
