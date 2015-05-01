83441b15b9a9756a2531b15b12c473b58bebfcf2
Rishabh Tayal <rtayal11@gmail.com>
2015-05-01 13:49:29 -0500

    added watch app target

---
+++ Code/VCinity WatchKit Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  VCinity WatchKit Extension
+//
+//  Created by Rishabh Tayal on 5/1/15.
+//  Copyright (c) 2015 Rishabh Tayal. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
---
+++ Code/VCinity WatchKit Extension/InterfaceController.m
@@ -0,0 +1,38 @@
+//
+//  InterfaceController.m
+//  VCinity WatchKit Extension
+//
+//  Created by Rishabh Tayal on 5/1/15.
+//  Copyright (c) 2015 Rishabh Tayal. All rights reserved.
+//
+
+#import "InterfaceController.h"
+
+
+@interface InterfaceController()
+
+@end
+
+
+@implementation InterfaceController
+
+- (void)awakeWithContext:(id)context {
+    [super awakeWithContext:context];
+
+    // Configure interface objects here.
+}
+
+- (void)willActivate {
+    // This method is called when watch view controller is about to be visible to user
+    [super willActivate];
+}
+
+- (void)didDeactivate {
+    // This method is called when watch view controller is no longer visible
+    [super didDeactivate];
+}
+
+@end
+
+
+
